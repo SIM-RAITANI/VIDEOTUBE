e3aa21eacca262c8ad110fd994469840323f19bd
SIM_RAITANI <simranraitani53@gmail.com>
2025-01-07 23:01:53 +0530

    Changes added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../components/Sidebar'
 const Home = () => {
   return (
     <>
-    <div className='flex h-[calc(100vh-4rem)] overflow-hidden'>
+    <div className='  flex h-[calc(100vh-4rem)] overflow-hidden'>
         <Sidebar/>
         <VideoGrid/>
     </div>
